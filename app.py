9b38e97b71f513e49a1c39e1539efd970aa98c3e
K. Ana Jessica <anajessica15022002@gmail.com>
2021-06-14 02:31:33 +0530

    changed values

--- app.py
+++ app.py
@@ -21,55 +21,25 @@ import species
 app  = Flask(__name__)
 PORT = 3000
 
+'''
+http://0.0.0.0:3000/
+'''
+
 @app.route("/", methods=["GET","POST"])
 def startpy():
-
-    result = {
-
-        "Greetings" : "Tactlabs welcomes you"
-    }
 
     # return jsonify(result)
     return render_template("index.html") 
 
 '''
-http://0.0.0.0:3091/api/data
+http://0.0.0.0:3000/api/data
 '''
 
-@app.route("/api/data", methods=["GET"])
+@app.route("/api/data", methods=["GET","POST"])
 def api_get_data():
 
     result = species.get_data()
-
-    # result_dict = {
-
-    #     'year'       : year,
-    #     'pytorch'    : pytorch,
-    #     'tensorFlow' : tensorFlow
-
-    # }
-
-    return jsonify(result)
-
-'''
-http://0.0.0.0:3091/api/add
-http://0.0.0.0:3091/api/add?year=2017&ontario_tourist=20345&quebec_tourist=200
-http://0.0.0.0:3000/api/add?year=2021&pytorch=180&tensorFlow=90
-'''
-@app.route("/api/add", methods=["GET"])
-def api_add_data():
-
-   # year            = request.values.get('year')
-    Treespecies = request.values.get('Tree species')
-    PercentageInOntario  = request.values.get('Percentage in Ontario')
-
-    result = {
-        'Tree species'       : Treespecies,
-        'Percentage in Ontario'    : PercentageInOntario,
-
-    }
-    result_data = species.add_row(Treespecies,PercentageInOntario )
-
+    print(result)
     return jsonify(result)
 
 
--- species.py
+++ species.py
@@ -4,42 +4,29 @@ def get_data():
     
     df = pd.read_csv('data.csv')
 
-    print(df['Tree Species'].tolist())
+    # print(df['Tree Species'].tolist())
 
-    TreeSpecies_data    = df['Tree Species'].tolist()
+    TreeSpecies_data           = df['Tree Species'].tolist()
 
-    PercentageInOntario_data = df['Percentage in Ontario'].tolist()
+    PercentageInOntario_data   = df['Percentage in Ontario'].tolist()
 
     # print(df['quebec'].tolist())
 
+    list_of_tuples = [tuple(row) for row in df.values]
+  
+    print(list_of_tuples)
+
     result_dict = {
-       
-        'Tree Species'    : TreeSpecies_data,
-        'Percentage in Ontario' : PercentageInOntario_data
+        'Tree Species'                  : TreeSpecies_data,
+        'Percentage in Ontario'         : PercentageInOntario_data,
+        'data_list'                     : list_of_tuples
     }
+
+    
 
     # print(result_dict)
 
     return result_dict
-
-def add_row(TreeSpecies, PercentageInOntario):
-
-    df = pd.read_csv('data.csv') 
-
-    new_row = {
-    
-        
-        'Tree Species'    : TreeSpecies, 
-        'Percentage in Ontario' : PercentageInOntario
-    }
-
-    print(df)
-
-    df = df.append(new_row, ignore_index=True)
-
-    print(df)
-
-    df.to_csv('data.csv')
 
 if __name__ == "__main__":
     get_data()
